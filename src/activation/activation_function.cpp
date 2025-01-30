a9ca7196dead44a65a4e66ae69fd8d915958c6df
JacopoRaffi <raffijacopo@gmail.com>
2025-01-30 22:38:16 +0100

    add implementation activation functions

--- src/activation/activation_function.cpp
+++ src/activation/activation_function.cpp
@@ -1,9 +1,38 @@
 #include "activation_function.hpp"
 
+// ---------------------------------------- Linear ----------------------------------------
+Eigen::VectorXd Linear::derivative(const Eigen::VectorXd& x){
+    return x;
+};
+
+Eigen::VectorXd Linear::derivative(const Eigen::VectorXd& x){
+    return Eigen::VectorXd::Ones(x.size());
+};
+
+
+// ---------------------------------------- RELU ----------------------------------------
 Eigen::VectorXd ReLU::activate(const Eigen::VectorXd& x){
     return x.array().max(0);
 };
 
 Eigen::VectorXd ReLU::derivative(const Eigen::VectorXd& x){
     return (x.array() > 0).cast<double>();
+};
+
+// ---------------------------------------- Sigmoid ----------------------------------------
+Eigen::VectorXd Sigmoid::activate(const Eigen::VectorXd& x){
+    return 1 / (1 + (-x.array()).exp());
+};
+
+Eigen::VectorXd Sigmoid::derivative(const Eigen::VectorXd& x){
+    return activate(x).array() * (1 - activate(x).array());
+};
+
+// ---------------------------------------- Tanh ----------------------------------------
+Eigen::VectorXd Tanh::activate(const Eigen::VectorXd& x){
+    return x.array().tanh();
+};
+
+Eigen::VectorXd Tanh::derivative(const Eigen::VectorXd& x){
+    return 1 - activate(x).array().square();
 };
