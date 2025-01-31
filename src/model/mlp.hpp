3327636d28b4fe57ae818ae03719f39b2c8a2ca7
JacopoRaffi <raffijacopo@gmail.com>
2025-01-31 19:23:28 +0100

    add MLP class definition

---
+++ src/model/mlp.hpp
@@ -0,0 +1,72 @@
+#ifndef MLP_HPP
+#define MLP_HPP
+
+#include <eigen3/Eigen/Dense>
+#include <vector>
+#include <string>
+#include <utility>
+#include "layer.hpp"
+
+/**
+ * @brief Multi-layer perceptron class
+ * 
+ * This class implements a multi-layer perceptron (MLP) model, trained with standard SGD.
+ */
+class MLP{
+private:
+    std::vector<FCLayer> layers;
+
+public:
+    /**
+     * @brief Construct a new MLP object
+     * 
+     * @param input_size Input size
+     * @param layers List of pairs of (number of neurons, activation function) for each layer. Last layer will be the output layer
+     */
+    MLP(int input_size, std::vector<std::pair<int, std::string>> layers);
+
+    /**
+     * @brief Re-Initialize weights and biases given the ranges. the i-th ranges is used for layer i
+     * 
+     * @param weight_ranges Weight ranges
+     * @param bias_ranges Bias ranges
+     */
+    void init_weights(std::vector<std::pair<int, int>> weight_ranges, std::vector<std::pair<int, int>> bias_ranges);
+
+    /**
+     * @brief Forward pass
+     * 
+     * @param x Input data
+     * @return Eigen::MatrixXd Output data
+     */
+    Eigen::MatrixXd predict(Eigen::MatrixXd x);
+
+    /**
+     * @brief Backward pass
+     * 
+     * @param loss_grad Gradient of the loss function
+     */
+    void backward(Eigen::MatrixXd loss_grad);
+
+    /**
+     * @brief Update weights
+     */
+    void update();
+
+    /**
+     * @brief Fit the model
+     * 
+     * @param x Input data
+     * @param y Target data
+     * @param epochs Number of epochs
+     * @param num_minibatches Number of minibatches
+     * @param learning_rate Learning rate
+     * @param weight_decay Weight decay
+     * @param momentum Momentum
+     */
+    void fit(Eigen::MatrixXd x, Eigen::MatrixXd y, int epochs, int num_minibatches, double learning_rate, double weight_decay, double momentum);
+
+     ~MLP();
+};
+
+#endif // MLP_HPP
