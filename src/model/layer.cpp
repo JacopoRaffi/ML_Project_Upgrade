09b706f6626c76286c4daffd8e93521d54553454
JacopoRaffi <raffijacopo@gmail.com>
2025-01-31 19:06:00 +0100

    add comments

--- src/model/layer.cpp
+++ src/model/layer.cpp
@@ -8,22 +8,24 @@ FCLayer::FCLayer(int input_size, int output_size, std::unique_ptr<ActivationFunc
             float min_val, float max_val, float bias_max_val, float bias_min_val){
     
     activation = std::move(func);
+
+    // Uniform random initialization in the range [min_val, max_val] both for weights and bias
     weights = min_val + (Eigen::MatrixXd::Random(output_size, input_size).array() + 1.0) * (max_val - min_val) / 2.0;
-    bias =  bias_min_val + (Eigen::MatrixXd::Random(output_size, 1).array() + 1.0) * (bias_max_val - bias_min_val) / 2.0;
+    bias =  bias_min_val + (Eigen::MatrixXd::Random(output_size, 1).array() + 1.0) * (bias_max_val - bias_min_val) / 2.0; 
 };
 
 Eigen::MatrixXd FCLayer::forward(const Eigen::MatrixXd& x){
     input = x;
-    output = x * weights.transpose() + bias.transpose().replicate(x.rows(), 1);
-    return activation->activate(output);
+    output = x * weights.transpose() + bias.transpose().replicate(x.rows(), 1); // X*W^T + b^T
+    return activation->activate(output); // activation(X*W^T + b^T)
 };
 
 Eigen::MatrixXd FCLayer::backward(const Eigen::MatrixXd& grad){
-    Eigen::MatrixXd delta = grad.cwiseProduct(activation->derivative(output)); 
-    grad_weights = delta.transpose() * input;
+    Eigen::MatrixXd delta = grad.cwiseProduct(activation->derivative(output)); // grad * activation'(output)
+    grad_weights = delta.transpose() * input; // delta^T * X
     grad_bias = delta.colwise().sum();
 
-    return delta * weights;
+    return delta * weights; // error to backpropagate
 };
 
 void FCLayer::update(double learning_rate, double weight_decay, double momentum){
@@ -36,18 +38,3 @@ void FCLayer::update(double learning_rate, double weight_decay, double momentum)
     prev_weights_update = weights_update;
     prev_bias_update = bias_update;
 };
-
-int main(){
-    MSE mse;
-    std::unique_ptr<ActivationFunction> activation(new Sigmoid());
-    FCLayer layer(2, 2, std::move(activation));
-    Eigen::MatrixXd x(2, 2);
-    x << 1, 2, 3, 4;
-    Eigen::MatrixXd y = layer.forward(x);
-
-    Eigen::MatrixXd y_true = Eigen::MatrixXd::Ones(2, 2);
-
-    Eigen::MatrixXd grad = mse.backward(y_true, y);
-    Eigen::MatrixXd delta = layer.backward(grad);
-    return 0;
-}
