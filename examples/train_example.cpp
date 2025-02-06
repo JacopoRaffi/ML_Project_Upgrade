f960d7983b540828b0432c711cfbad376149c1f1
JacopoRaffi <raffijacopo@gmail.com>
2025-02-06 14:36:47 +0100

    completed example of a training

--- examples/train_example.cpp
+++ examples/train_example.cpp
@@ -32,9 +32,11 @@ void write_to_file(std::vector<std::pair<double, double>> loss_history, int epoc
  * @return Eigen::MatrixXd the target data
  */
 Eigen::MatrixXd synthetic_target(Eigen::MatrixXd input){
-    Eigen::MatrixXd target = Eigen::MatrixXd::Zero(input.rows(), 1);
+    Eigen::MatrixXd target = Eigen::MatrixXd::Zero(input.rows(), 3);
     for(int i = 0; i < input.rows(); i++){
         target(i, 0) = 2*input(i, 0) + 3*input(i, 1) + 4*input(i, 2) + 5*input(i, 3) + 6*input(i, 4); // y = 2x1 + 3x2 + 4x3 + 5x4 + 6x5
+        target(i, 1) = 3*input(i, 0) + 4*input(i, 1) + 5*input(i, 2) + 6*input(i, 3) + 7*input(i, 4); // y = 3x1 + 4x2 + 5x3 + 6x4 + 7x5
+        target(i, 2) = 4*input(i, 0) + 5*input(i, 1) + 6*input(i, 2) + 7*input(i, 3) + 8*input(i, 4); // y = 4x1 + 5x2 + 6x3 + 7x4 + 8x5
     }
     return target;
 }
@@ -55,9 +57,11 @@ int main(int argc, char* argv[]){
     std::vector<std::pair<int, ActivationFunction*>> layers;
 
     layers.push_back(std::make_pair(50, activation));
-    layers.push_back(std::make_pair(1, output_activation));
+    layers.push_back(std::make_pair(50, activation));
+    layers.push_back(std::make_pair(3, output_activation));
 
-    MLP mlp(num_features, layers);
+    // simple MLP with 2 hidden layers (50 neuron each and Sigmoid activation) and a linear output layer (3 neurons)
+    MLP mlp(num_features, layers); 
 
     MSE mse;
     int epochs = 10;
